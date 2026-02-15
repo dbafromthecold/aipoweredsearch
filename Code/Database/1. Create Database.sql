c72cc54972072ad916da6fea88d457dd75816fae
Andrew Pruski <dbafromthecold@gmail.com>
2026-02-15 14:00:11 +0000

    added comment headers to demo scripts

--- Code/Database/1. Create Database.sql
+++ Code/Database/1. Create Database.sql
@@ -1,3 +1,15 @@
+/***************************************************************************
+***************************************************************************
+* AI-Powered Search - Andrew Pruski
+* @dbafromthecold.com
+* dbafromthecold@gmail.com
+* https://github.com/dbafromthecold/aipoweredsearch
+* Create database
+***************************************************************************
+**************************************************************************/
+
+
+
 USE [master]
 GO
 
@@ -21,22 +33,4 @@ CREATE DATABASE [burrito-bot-db]
 ( NAME = N'burrito-bot-db-raw-data',	FILENAME = N'F:\SQLData1\burrito-bot-db-raw-data.ndf' ,		SIZE = 1048576KB , MAXSIZE = UNLIMITED, FILEGROWTH = 524288KB )
  LOG ON 
 ( NAME = N'burrito-bot-db_log',			FILENAME = N'G:\SQLTLog1\burrito-bot-db_log.ldf' ,			SIZE = 5242880KB , MAXSIZE = UNLIMITED, FILEGROWTH = 524288KB )
-GO
-
-
-
-
-
-CREATE TABLE [raw_data].[mexican_restaurants_Belfast](
-	[place_id]			[nvarchar](50) NOT NULL,
-	[name]				[nvarchar](50) NOT NULL,
-	[rating]			[float] NOT NULL,
-	[review_count]		[smallint] NOT NULL,
-	[price_level]		[tinyint] NULL,
-	[address]			[nvarchar](100) NOT NULL,
-	[phone_number]		[nvarchar](50) NULL,
-	[latitude]			[float] NOT NULL,
-	[longitude]			[float] NOT NULL,
-	[business_status]	[nvarchar](50) NOT NULL
-) --ON [RAW_DATA]
 GO
