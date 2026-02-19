1be5fefefcec4f9db731dc721cfa44ec49f85582
Andrew Pruski <dbafromthecold@gmail.com>
2026-02-19 11:57:22 +0000

    updated script to export for visualisation

--- Data/Visualise Data/Export-TSV.ps1
+++ Data/Visualise Data/Export-TSV.ps1
@@ -5,7 +5,7 @@ $Server = "localhost"
 $Database = "burrito-bot-db"
 
 $Query = "SELECT CAST(embeddings AS NVARCHAR(MAX)) AS Embedding
-FROM dbo.restaurants
+FROM [embeddings].[restaurant_review_embeddings]
 ORDER BY Id"
 
 $Rows = Invoke-SqlCmd -ServerInstance $Server -Database $Database -Query $Query -MaxCharLength 1000000
@@ -27,20 +27,37 @@ $rows = Invoke-Sqlcmd `
     -ServerInstance $Server `
     -Database $Database `
     -Query "
-        SELECT [name], [city], [rating], [review_count], [address], [phone]
-        FROM dbo.restaurants
-        ORDER BY Id
+SELECT
+    CONCAT(
+        d.name, ' is a Mexican restaurant in ', d.city, '. ',
+        'Customer reviews say: ',
+        STRING_AGG(
+            REPLACE(
+                REPLACE(rv.review_text, CHAR(13), ' '),
+                CHAR(10), ' '
+            ),
+            ' | '
+        ) WITHIN GROUP (ORDER BY rv.review_published_utc)
+    ) AS metadata_text
+FROM [data].[reviews] rv
+INNER JOIN [data].[restaurants] d ON rv.restaurant_id = d.id
+GROUP BY rv.restaurant_id, d.name, d.city;
     "
 
-$rows |
-ForEach-Object {
-    @(
-        $_.name,
-        $_.city,
-        $_.rating,
-        $_.review_count,
-        $_.address,
-        $_.phone
-    ) -join "`t"
-} |
-Set-Content "C:\temp\restaurant_metadata.tsv"
+$rows | Select-Object -ExpandProperty metadata_text |
+    Set-Content "C:\temp\restaurant_metadata.tsv"
+
+
+
+##$rows |
+##ForEach-Object {
+##   @(
+##        $_.name,
+##        $_.city,
+##        $_.rating,
+##        $_.review_count,
+##        $_.address,
+##        $_.phone
+ ##   ) -join "`t"
+##} |
+##Set-Content "C:\temp\restaurant_metadata.tsv"
