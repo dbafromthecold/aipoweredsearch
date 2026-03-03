334695ab3799485177b9131433955e70c2c836e6
Andrew Pruski <dbafromthecold@gmail.com>
2026-03-03 19:19:34 +0000

    added demos to pull data

---
+++ Code/Database/3b. Pull Restaurant Reviews.sql
@@ -0,0 +1,45 @@
+/**************************************************************************
+***************************************************************************
+* AI-Powered Search - Andrew Pruski
+* @dbafromthecold.com
+* dbafromthecold@gmail.com
+* https://github.com/dbafromthecold/aipoweredsearch
+* Pull restaurant reviews
+***************************************************************************
+**************************************************************************/
+
+
+
+-- declare variables
+DECLARE @ApiKey     VARCHAR(100) = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+DECLARE @PlaceId    VARCHAR(50)  = 'ChIJYczaQt4PZ0gRsN10i1TxJk8';
+DECLARE @FullUrl    VARCHAR(500);
+
+
+
+-- construct url
+SET @FullUrl = 'https://places.googleapis.com/v1/places/' + @PlaceId;
+
+DECLARE @Headers NVARCHAR(MAX) =
+'{"X-Goog-Api-Key":"' + @ApiKey +
+'","X-Goog-FieldMask":"reviews"}';
+
+DECLARE @ret1 INT, @response1 NVARCHAR(MAX);
+
+
+
+-- pull review data
+EXEC @ret1 = sp_invoke_external_rest_endpoint
+    @url = @FullUrl,
+    @headers = @Headers,
+    @method = 'GET',
+    @response = @response1 OUTPUT;
+--SELECT @response1;
+
+
+
+-- parse results
+SELECT [review]
+FROM OPENJSON(@response1, '$.result.reviews')
+WITH ([review] NVARCHAR(MAX) '$.text.text');
+GO
